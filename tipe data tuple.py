2b0a5befbb9d13ac273ba89a5da5d6f1a9b71036
Serlia-could <serlia121204@gmail.com>
2024-10-25 13:30:29 +0800

    Add files via upload

---
+++ tipe data tuple.py
@@ -0,0 +1,21 @@
+main  = "tipe data tuple sangat mirip dengan data list \ncontoh"
+print (main)
+tuple = (3, 4,7,"Shalom", "false")
+print (tuple)
+
+tuple = ["halo", "selamat","siang","Shalom", "false"]
+print (type(tuple))
+       
+tuple = ("halo", "selamat","siang","Shalom", "false")
+print (type(tuple))
+
+A2= ("Python", 30, 7.20, True, 'Serlia Turu Allo', 3j)
+print(A2[0])
+print(A2[1])
+print(A2[2])
+print(A2[2:5])
+print(A2[:3])
+print(A2[3:])
+print(A2[:])
+print (A2[3:3])
+
