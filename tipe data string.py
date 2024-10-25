2d7948d6005a703d78a8319dd38bdef7541c9a7b
Serlia-could <serlia121204@gmail.com>
2024-10-25 11:09:39 +0800

    Add files via upload

---
+++ tipe data string.py
@@ -0,0 +1,14 @@
+perkenalan = "Halo nama saya serlia turu allo"
+print (perkenalan)
+alamat = 'bersal dari mamuju'
+print (alamat)
+hoby = '''Suka menganalisis\ndan badminton'''
+print (hoby)
+teks = "mengubah format tanggal"
+print (teks)
+tanggal_awal = "2024-10-25"
+tahun, bulan, hari = tanggal_awal.split("-")
+tanggal_baru = f"{hari}/{tahun}/{bulan}"
+print (tanggal_baru)
+ucapanpenutup = '''sekian dari saya mengenai bahasa string , saya ucapkan banyak\nbanyak terimakasih'''
+print (ucapanpenutup)
