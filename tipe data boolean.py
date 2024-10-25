2342d09f13e75c900af09c49a8855851e9e85941
Serlia-could <serlia121204@gmail.com>
2024-10-25 12:43:27 +0800

    Add files via upload

---
+++ tipe data boolean.py
@@ -0,0 +1,27 @@
+tipedatabolean = "tipe data boolean hanya bisa di isi dengan nilai \ntrue dan false"
+print (tipedatabolean)
+
+kurangdari = 20 < 10
+print (kurangdari)
+besardari = 30 > 5
+print (besardari)
+hasilbesardari = "A" == "B"
+print (hasilbesardari)
+
+y = 7
+x = 8
+hasil = y < x
+print (hasil)
+
+umur = 19 
+if umur >=15:
+    print ("saya belum bisa memiliki KTP")
+else:
+    print ("saya belum bisa memiliki KTP")
+
+bb = 45
+if bb <=40:
+    print ("maka saya harus makan lebih banyak")
+else:
+    print ("maka saya harus makan lebih banyak")
+    
