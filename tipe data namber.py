32b1804ba13301b89bd4f7bc6f95cd5c42b0898a
Serlia-could <serlia121204@gmail.com>
2024-10-25 12:07:50 +0800

    tipe data namber.py

---
+++ tipe data namber.py
@@ -0,0 +1,32 @@
+# integer (bilangan bulat)
+Umur = 19 
+tahun = 2024
+print (Umur)
+print (tahun)
+hp = 20 
+print (hp)
+
+# float (desimal)
+tb = 156,8
+harga_cabe = 250.250
+y = 33.10 + 3.6
+print (tb)
+print (harga_cabe)
+print (y)
+a = 20
+b = 6
+c = a / b
+print (c)
+
+# bilangan complex 
+y = 2j 
+x = 3 + 5j
+
+l = 3 - 30j
+m = 60 * 10
+o = 250 / 5
+
+print (y + x)
+print (l - m * o)
+print (l / m)
+
