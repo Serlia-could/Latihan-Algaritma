45a6f14dcfc15a03277792978ce7cd2b90c8f6f7
Serlia-could <serlia121204@gmail.com>
2024-10-25 13:11:07 +0800

    Add files via upload

---
+++ tipe data list.py
@@ -0,0 +1,18 @@
+pendahuluan = "tipe data list adalah urutan yang dapat kita list"
+print (pendahuluan)
+sayur = ["sawi", "tomat", "cabe", "dll"]
+buah = ["apel", "jeruk", "mangga", "banana"]
+bar = ["15", "20", "40"]
+
+print (sayur)
+print (buah)
+print (bar)
+
+campuran = ["'ayo", "kita", "belajar", "python", "di", "dunia", "ikom"]
+print (campuran)
+
+fruits = ["apel", "pisang" " jeruk"]
+fruits.append("semangka")
+fruits.remove("apel")
+for fruit in fruits:
+    print (fruits)
