a9b34eaf3bd75131babbd938d9213635c4cd896f
Serlia-could <serlia121204@gmail.com>
2024-10-26 11:12:01 +0800

    Add files via upload

---
+++ operator logika.py
@@ -0,0 +1,18 @@
+b1 = (8 > 50) and (5 <= 8)
+print(b1)
+ 
+b2 = ('serlia' == 'sasha') or (10 <= 8)
+print(b2)
+ 
+b3 = not (10 < 10)
+print(b3)
+ 
+b4 = ('yadit' == 'dfina') and (10 <= 8) or (1 != 1)
+print(b4)	
+print('Hasil dari True and True   :', True and True)
+print('Hasil dari True or False  :', True or False)
+print('Hasil dari False and True  :', False and True)
+print('Hasil dari False or False :', False or False)
+ 
+print('\n')
+ 
