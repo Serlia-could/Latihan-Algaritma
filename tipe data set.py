95b2d19411cb581c728681f4c52460b3afca2948
Serlia-could <serlia121204@gmail.com>
2024-10-26 09:06:56 +0800

    Add files via upload

---
+++ tipe data set.py
@@ -0,0 +1,21 @@
+set = "selain tuple tipe data set juga hampir sama dengan tipe daya list"
+s2 = ["aku", "suka", "bljar", "python"]
+print(type(s2))
+ 
+s3 = ("karena", "dapat", "menambah", "wawasan")
+print(type(s3))
+ 
+s4 = {"selain", "itu", "python", "juga", "sangat", "penting"} 
+print(type(s4))
+
+list = {1, 2, 3, 4, 5, "serlia", "sasha"}
+print (list)
+
+list.add("yadit")
+print (list)
+list.remove("sasha")
+print (list)
+list.pop()
+print (list)
+list.clear()
+print (list)
