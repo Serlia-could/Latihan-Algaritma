c768b093c9c04321b450ae4bbc395a139ace44c1
Serlia-could <serlia121204@gmail.com>
2024-10-26 09:25:11 +0800

    Add files via upload

---
+++ tipe data dictionary.py
@@ -0,0 +1,16 @@
+dictionary = '''Tipe data dictionary ini cocok dipakai untuk kelompok data yang kompleks'''
+print (dictionary)
+nama = {
+    "serlia": "reza",
+    "yadit": "elvi",
+    "isda": "nesa",
+}
+print (nama)
+dictionary = {
+    "Tipe": "data dictionary",
+    "adalah tipe data": "array",
+    "dimana key dan index": "Tipe data dictionary",
+    "bisa berbentuk string": "tidak",
+    "hanya number": "saja",
+}
+print (dictionary)
