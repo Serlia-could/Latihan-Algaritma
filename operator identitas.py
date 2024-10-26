b066c0dcad37ca13e19522d9151364be5d1428a8
Serlia-could <serlia121204@gmail.com>
2024-10-26 11:43:05 +0800

    Add files via upload

---
+++ operator identitas.py
@@ -0,0 +1,20 @@
+serlia = 'serlia Mahasiswa Universitas Sulawesi Barat'
+print('serlia :',serlia)
+print('\'i\' in serlia     :', 'i' in serlia)
+print('\'k\' not in serlia :', 'k' not in serlia)
+print('\'d\' not in serlia :', 'd' not in serlia)
+print('\n')
+ 
+ 
+turu = ['a','b','c']
+print('bar :',turu)
+print('\'a\' in turu     :', 'a' in turu)
+print('\'a\' not in turu :', 'a' not in turu)
+print('\'d\' not in turu :', 'd' not in turu)
+print('\n')
+ 
+allo = (12,43,102,55)
+print('baz :',allo)
+print('102 in allo     :', 102 in allo)
+print('102 not in allo :', 102 not in allo)
+print('35 not in allo  :', 35 in allo)
