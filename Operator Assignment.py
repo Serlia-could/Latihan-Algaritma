f1a294f48784f15331a679463a1d74a8237e2dea
Serlia-could <serlia121204@gmail.com>
2024-10-26 11:34:28 +0800

    Add files via upload

---
+++ Operator Assignment.py
@@ -0,0 +1,29 @@
+operator_assigbment = "Operator assignment adalah operator untuk \nmemasukkan suatu nilai ke dalam variabel"
+print (operator_assigbment)
+a = 9
+b = 250
+b = b + 1
+c = a - b
+d = c + c + a - a
+e = (c + d)* a
+ 
+print('mengisi a:',a)
+print('mengisi b:',b)
+print('mengisil c:',c)
+print('mengisi d:',d)
+print('mengisi e:',e)
+x = 60
+x += 90
+print('x += 9  :',x)
+  
+x = 60
+x /= 90
+print('x /= 7  :',x)
+  
+x = 60
+x **= 90
+print('x **= 8 :',x)
+  
+x = 60
+x <<= 90
+print('x <<= 2 :',x)
