5083607642d8e4725cbd04eba9b33a46cc3e7362
Serlia-could <serlia121204@gmail.com>
2024-10-26 11:01:15 +0800

    Add files via upload

---
+++ perbandingan rasional.py
@@ -0,0 +1,13 @@
+x = 80
+y = 50
+ 
+print('x =',x)
+print('y =',y)
+print('\n')
+ 
+print('x == y hasilnya',x==y)
+print('x != y hasilnya',x!=y)
+print('x > y  hasilnya',x>y)
+print('x < y  hasilnya',x<y)
+print('x >= y hasilnya',x>=y)
+print('x <= y hasilnya',x<=y)
